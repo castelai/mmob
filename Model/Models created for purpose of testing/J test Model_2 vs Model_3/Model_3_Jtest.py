d7a954918f20ab50193cfe5db92407645d4075d5
Valchy94 <valentina.gnamus@gmail.com>
2018-12-14 15:03:07 +0100

    Test models improved

--- Model/Models created for purpose of testing/J test Model_2 vs Model_3/Model_3_Jtest.py
+++ Model/Models created for purpose of testing/J test Model_2 vs Model_3/Model_3_Jtest.py
@@ -182,9 +182,7 @@ V = {1: CAR, 2: SM, 0: PT}
 av = {1: one, 2: one, 0: one}
 
 # EXCLUDE
-
-BIOGEME_OBJECT.EXCLUDE = (Choice == -1)
-
+BIOGEME_OBJECT.EXCLUDE = (Choice == -1) + (BirthYear == -1) + (Income == -1) # I exclude the respondents whose age we can not calculate or whose income we don't know
 # MNL (Multinomial Logit model), with availability conditions
 logprob = bioLogLogit(V,av,Choice)
 
